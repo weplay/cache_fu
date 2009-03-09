644a288788fce1e554d24e039da1435711b4bc74
Bryan Helmkamp <bryan@brynary.com>
2009-03-09 14:19:29 -0400

    Don't require memcache.
    Rails does this because our cache_store is set to mem_cache_store in environment.rb

--- init.rb
+++ init.rb
@@ -1,19 +1,19 @@
-begin
-  require 'memcache'
-rescue LoadError
-end
-
-begin
-  require 'memcached'
-rescue LoadError
-end
-
-begin
-  require 'mem_cache_with_consistent_hashing'
-rescue LoadError
-end
-
-puts "=> You should be using the `memcache-client' gem.  You're using RubyMemcache!" if Object.const_defined?(:RubyMemcache)
+# begin
+#   require 'memcache'
+# rescue LoadError
+# end
+# 
+# begin
+#   require 'memcached'
+# rescue LoadError
+# end
+# 
+# begin
+#   require 'mem_cache_with_consistent_hashing'
+# rescue LoadError
+# end
+# 
+# puts "=> You should be using the `memcache-client' gem.  You're using RubyMemcache!" if Object.const_defined?(:RubyMemcache)
 
 require 'acts_as_cached'
 
